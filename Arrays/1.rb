0255e66cc7d2452d15a7eb6e3fd0deb5c70c0630
Nikema Prophet <prophen@users.noreply.github.com>
2016-07-18 09:10:49 -0700

    Arrays Chapter Exercises Completed

---
+++ Arrays/1.rb
@@ -0,0 +1,10 @@
+# Exercise 1 
+
+arr = [1, 3, 5, 7, 8 , 11]
+number = 3
+
+if arr.include?(number)
+  puts "#{number} is in the array"
+else
+  puts "#{number} is not in the array"
+end
---
+++ Arrays/7.rb
@@ -0,0 +1,11 @@
+# Exercise 7
+
+arr = [2, 4, 6, 8]
+new_arr = []
+
+arr.each do |n|
+  new_arr << n + 2
+end
+
+p arr
+p new_arr
